1f9d3299395c7654b7d2641d9550703e94694c71
Aaron M. Agosto <aaronagosto0613@gmail.com>
2023-03-23 18:28:09 -0400

    ADDED homework

---
+++ homework/weekOneDayFour.js
@@ -0,0 +1,28 @@
+// question1 
+ 'declare'
+
+ //question2
+ 'invoke'
+
+ //question3
+
+const student = {
+ gender: 'male',
+ hair: 'bald',
+ age: '21'
+}
+
+//question4 Create a function that removes the first element of the array below and adds “kiwi” to the end of the array.
+ let favoriteFruits = [
+    'mango', 
+    'lychee', 
+    'strawberry', 
+    'papaya'
+] 
+function otherFruits(favoriteFruits) {
+     favoriteFruits.shift()
+     favoriteFruits.push('kiwi')
+     console.log(favoriteFruits)
+
+}
+otherFruits(favoriteFruits,)
